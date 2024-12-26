26619a952e1128f026043722f9d6f37066e2b5ac
xingdong qin <17611305779@163.com>
2024-12-26 23:28:37 +0800

    day2

---
+++ src/gocode/project01/helloWorld.go
@@ -0,0 +1,5 @@
+package main	//声明文件所在的包，每个go文件必须有归属地包
+import "fmt"
+func main(){
+	fmt.Prontln("Hello World!!!")
+}
---
+++ src/gocode/project01/unit02/demo01/main.go
@@ -0,0 +1,13 @@
+package main
+import fmt
+
+func main(){
+	//1.变量的声明
+	var age int
+	//2.变量的赋值
+	age = 18	
+	//3.变量的使用
+	fmt.Println("age=",age)
+}
+
+
